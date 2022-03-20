57f8429b45290c7bdca4811537716ea2eb71b6ff
Forrest Miller <forrestmillerj@gmail.com>
2022-03-20 18:46:50 -0500

    tgswitch correct version

--- scripts/install_tools.sh
+++ scripts/install_tools.sh
@@ -26,7 +26,7 @@ tfswitch 1.1.6
 echo "======> Downloading and installing tgswitch"
 curl -L https://raw.githubusercontent.com/warrensbox/tgswitch/release/install.sh | bash
 echo "======> Installing terragrunt 0.36.1"
-tgswitch 1.1.6
+tgswitch 0.36.1
 
 
 # install aws
