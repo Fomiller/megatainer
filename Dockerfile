b985a0bf5912a5f452fd3e1870c258c9670eb8d7
Forrest Miller <forrestmillerj@gmail.com>
2022-03-20 12:33:11 -0500

    moved all installs into install script

--- Dockerfile
+++ Dockerfile
@@ -1,34 +1,12 @@
 FROM ubuntu:latest
 
-RUN apt-get update && apt-get upgrade
+ARG USER=root
 
 ENV PATH=$PATH:/usr/local/go/bin:/root/.please/bin
-# install utility dependecies
-RUN apt-get install -y wget
-RUN apt-get install -y curl
-RUN apt-get install -y unzip && apt-get install -y zip
+ENV AWS_DEFAULT_PROFILE="saml"
 
-# install python3.9
-RUN apt-get update && apt-get install -y software-properties-common gcc && add-apt-repository -y ppa:deadsnakes/ppa
-RUN apt-get update && apt-get install -y python3.9 python3-distutils python3-pip python3-apt
-RUN apt-get install python-is-python3
-
-# install golang
-RUN wget https://golang.org/dl/go1.18.linux-amd64.tar.gz
-RUN rm -rf /usr/local/go && tar -C /usr/local -xzf go1.18.linux-amd64.tar.gz
-
-# install terraform
-RUN curl -L https://raw.githubusercontent.com/warrensbox/terraform-switcher/release/install.sh | bash
-
-# install aws
-RUN curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-RUN unzip awscliv2.zip
-RUN ./aws/install
-
-# install please.build
 COPY ./scripts/install_tools.sh .
-# ENV PATH=$PATH:/root/.please/bin
+
 RUN chmod +x ./install_tools.sh && \
   ./install_tools.sh && \
   rm ./install_tools.sh
-
--- scripts/install_tools.sh
+++ scripts/install_tools.sh
@@ -1,4 +1,37 @@
 #!/usr/bin/env bash
+
+# install utility dependecies
+echo "======> Downloading and installing utilities"
+apt-get update && apt-get upgrade
+apt-get install -y wget
+apt-get install -y curl
+apt-get install -y unzip && apt-get install -y zip
+
+# install python3.9
+echo "======> Downloading and installing python"
+apt-get update && apt-get install -y software-properties-common gcc && add-apt-repository -y ppa:deadsnakes/ppa
+apt-get update && apt-get install -y python3.9 python3-distutils python3-pip python3-apt
+apt-get install python-is-python3
+
+# install golang
+echo "======> Downloading and installing python"
+wget https://golang.org/dl/go1.18.linux-amd64.tar.gz
+rm -rf /usr/local/go && tar -C /usr/local -xzf go1.18.linux-amd64.tar.gz
+
+# install terraform
+echo "======> Downloading and installing tfswitch"
+curl -L https://raw.githubusercontent.com/warrensbox/terraform-switcher/release/install.sh | bash
+echo "======> Downloading and installing tgswitch"
+curl -L https://raw.githubusercontent.com/warrensbox/tgswitch/release/install.sh | bash
+
+# install aws
+echo "======> Downloading and installing aws cli"
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+./aws/install
+
+#install please.build
+echo "======> Downloading and installing please.build"
 curl https://get.please.build > please.sh
 sh ./please.sh
 source ~/.profile
