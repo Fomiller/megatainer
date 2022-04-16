72a3725ee220db7f6c4e5e629fb8b582d17f7602
Forrest Miller <forrestmillerj@gmail.com>
2022-04-16 15:44:25 -0500

    multi arch build init

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM ubuntu:latest
 
 ARG USER=root
 
-ENV PATH=$PATH:/usr/local/go/bin:/root/.please/bin
+ENV PATH=$PATH:/usr/local/go/bin:/root/.please/bin:/.local/bin
 ENV AWS_DEFAULT_PROFILE="saml"
 
 COPY ./scripts/install_tools.sh .
@@ -10,3 +10,11 @@ COPY ./scripts/install_tools.sh .
 RUN chmod +x ./install_tools.sh && \
   ./install_tools.sh && \
   rm ./install_tools.sh
+
+# skipping plz install until further arm64 support
+# COPY ./scripts/plz_install.sh .
+# COPY ./scripts/plzw.sh .
+
+# RUN chmod +x ./plz_install.sh && \
+#   ./plz_install.sh && \
+#   rm ./plz_install.sh
--- scripts/install_tools.sh
+++ scripts/install_tools.sh
@@ -1,45 +1,78 @@
 #!/usr/bin/env bash
+OS=`uname`
+if [ "$OS" = "Linux" ]; then
+    GOOS="linux"
+elif [ "$OS" = "Darwin" ]; then
+    GOOS="darwin"
+elif [ "$OS" = "FreeBSD" ]; then
+    GOOS="freebsd"
+else
+    echo "Unknown operating system $OS"
+    exit 1
+fi
+
+ARCH=`uname -m`
+if [ "$ARCH" = "x86_64" ]; then
+    ARCH="amd64"
+elif [ "$ARCH" = "arm64"] || [ "${ARCH}" = "aarch64" ]; then
+    ARCH="arm64"
+else
+    echo "Unsupported cpu arch $ARCH"
+    exit 1
+fi
+echo "Running on ${ARCH}"
 
 # install utility dependecies
-echo "======> Downloading and installing utilities"
+# echo "======> Downloading and installing utilities"
 apt-get update && apt-get upgrade
 apt-get install -y wget
 apt-get install -y curl
 apt-get install -y unzip && apt-get install -y zip
 
-# install python3.9
+# # install python3.9
 echo "======> Downloading and installing python"
 apt-get update && apt-get install -y software-properties-common gcc && add-apt-repository -y ppa:deadsnakes/ppa
 apt-get update && apt-get install -y python3.9 python3-distutils python3-pip python3-apt
 apt-get install python-is-python3
 
-# install golang
-echo "======> Downloading and installing python"
-wget https://golang.org/dl/go1.18.linux-amd64.tar.gz
-rm -rf /usr/local/go && tar -C /usr/local -xzf go1.18.linux-amd64.tar.gz
+# # install golang
+# echo "======> Downloading and installing golang"
+wget https://golang.org/dl/go1.18.linux-${ARCH}.tar.gz
+rm -rf /usr/local/go && tar -C /usr/local -xzf go1.18.linux-${ARCH}.tar.gz
 
 # install terraform
 echo "======> Downloading and installing tfswitch"
 curl -L https://raw.githubusercontent.com/warrensbox/terraform-switcher/release/install.sh | bash
 echo "======> Installing terraform 1.1.6"
 tfswitch 1.1.6
+
+# install terragrunt
 echo "======> Downloading and installing tgswitch"
 curl -L https://raw.githubusercontent.com/warrensbox/tgswitch/release/install.sh | bash
 echo "======> Installing terragrunt 0.36.1"
 tgswitch 0.36.1
 
 
-# install aws
+# # install aws
 echo "======> Downloading and installing aws cli"
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-./aws/install
+if [ "$ARCH" = "x86_64"]; then
+  curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+  unzip awscliv2.zip
+  sh ./aws/install
+else
+  curl "https://awscli.amazonaws.com/awscli-exe-linux-aarch64.zip" -o "awscliv2.zip"
+  unzip awscliv2.zip
+  sh ./aws/install
+fi
 
-#install please.build
-echo "======> Downloading and installing please.build"
-curl https://get.please.build > please.sh
-sh ./please.sh
-source ~/.profile
-echo 'source <(plz --completion_script)' > ~/.bashrc
-rm -f please.sh
-plz --version
+# #install please.build
+# echo "======> Downloading and installing please.build"
+# please install script does not work on arm64, but i think it is still possible to install
+# please is not compatible with aarch64 architecture even though arm64 and aarch64 are the same
+# curl https://get.please.build > please.sh
+# ls
+# sh ./please.sh
+# source ~/.profile
+# echo 'source <(plz --completion_script)' > ~/.bashrc
+# rm -f please.sh
+# plz --version
