77e73f5746defab44122b5033deec91f23e9ec27
Forrest Miller <forrestmillerj@gmail.com>
2023-11-22 20:43:39 -0600

    install doppler

--- scripts/install_tools.sh
+++ scripts/install_tools.sh
@@ -31,3 +31,11 @@ echo "======> Downloading and installing tgswitch"
 curl -L https://raw.githubusercontent.com/warrensbox/tgswitch/release/install.sh | bash
 echo "======> Installing terragrunt ${TG_VERSION}"
 tgswitch ${TG_VERSION}
+
+# install doppler
+echo "======> Downloading and installing dopper"
+apt-get update && apt-get install -y apt-transport-https ca-certificates curl gnupg
+curl -sLf --retry 3 --tlsv1.2 --proto "=https" 'https://packages.doppler.com/public/cli/gpg.DE2A7741A397C129.key' | gpg --dearmor -o /usr/share/keyrings/doppler-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/doppler-archive-keyring.gpg] https://packages.doppler.com/public/cli/deb/debian any-version main" | tee /etc/apt/sources.list.d/doppler-cli.list
+apt-get update && apt-get install doppler
+
