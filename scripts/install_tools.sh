204a2beda914398c0629f03c1d30245b0f4452f5
Forrest Miller <forrestmillerj@gmail.com>
2023-12-01 07:48:53 -0600

    install kubectl

--- scripts/install_tools.sh
+++ scripts/install_tools.sh
@@ -33,9 +33,12 @@ echo "======> Installing terragrunt ${TG_VERSION}"
 tgswitch ${TG_VERSION}
 
 # install doppler
-echo "======> Downloading and installing dopper"
+echo "======> Downloading and installing doppler"
 apt-get update && apt-get install -y apt-transport-https ca-certificates curl gnupg
 curl -sLf --retry 3 --tlsv1.2 --proto "=https" 'https://packages.doppler.com/public/cli/gpg.DE2A7741A397C129.key' | gpg --dearmor -o /usr/share/keyrings/doppler-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/doppler-archive-keyring.gpg] https://packages.doppler.com/public/cli/deb/debian any-version main" | tee /etc/apt/sources.list.d/doppler-cli.list
 apt-get update && apt-get install doppler
 
+# install kubectl
+echo "======> Downloading and installing kubectl"
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/${ARCH}/kubectl"
